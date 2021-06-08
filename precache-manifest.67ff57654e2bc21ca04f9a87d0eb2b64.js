45e1ded682ebbbcaf5f44add75a95d45c701eb15
Cheng-Yang Tsai <greydust@gmail.com>
2021-06-08 22:11:29 +0800

    Updates

---
+++ precache-manifest.67ff57654e2bc21ca04f9a87d0eb2b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "953b64490c52325c77ac",
+    "url": "/umamusume/static/css/main.1cb6f8a4.chunk.css"
+  },
+  {
+    "revision": "953b64490c52325c77ac",
+    "url": "/umamusume/static/js/main.953b6449.chunk.js"
+  },
+  {
+    "revision": "7008bc176edc6ec9a285",
+    "url": "/umamusume/static/css/1.941c8575.chunk.css"
+  },
+  {
+    "revision": "7008bc176edc6ec9a285",
+    "url": "/umamusume/static/js/1.7008bc17.chunk.js"
+  },
+  {
+    "revision": "4ab5a9fff5d9bdc746a2",
+    "url": "/umamusume/static/js/runtime~main.4ab5a9ff.js"
+  },
+  {
+    "revision": "7dc9abc07a5eae4e5d1fe5a6a52b550e",
+    "url": "/umamusume/index.html"
+  }
+];
