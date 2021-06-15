fe8eb98d3104e9bdbf25edbdc7ff9a2b0bf64dc8
Cheng-Yang Tsai <greydust@gmail.com>
2021-06-15 20:45:26 +0800

    localization refactor

--- src/localization.js
+++ src/localization.js
@@ -1,19 +1,31 @@
 import _ from 'lodash';
 
 class Localization {
+  static getLocalizationJaJp() {
+    return require.context('./localization/', true, /ja-jp\.json$/);
+  }
+
+  static getLocalizationEnUs() {
+    return require.context('./localization/', true, /en-us\.json$/);
+  }
+
+  static getLocalizationZhTw() {
+    return require.context('./localization/', true, /zh-tw\.json$/);
+  }
+
   constructor() {
-    const files = require.context('./localization/', true, /ja-jp\.json$/);
-    const filesJson = _.map(files.keys(), (key) => files(key));
-    this.default = _.merge({}, ...filesJson);
+    this.default = {};
+    this.localizer = {
+      'ja-jp': Localization.getLocalizationJaJp,
+      'en-us': Localization.getLocalizationEnUs,
+      'zh-tw': Localization.getLocalizationZhTw,
+    };
+    this.default = this.getLocalization('ja-jp');
   }
 
   getLocalization(locale) {
-    const files = require.context('./localization/', true, /.json$/);
-    const filesJson = _
-      .chain(files.keys())
-      .filter((key) => _.includes(key, locale))
-      .map((key) => files(key))
-      .value();
+    const files = this.localizer[locale]();
+    const filesJson = _.map(files.keys(), (key) => files(key));
     return _.merge({}, this.default, ...filesJson);
   }
 }
