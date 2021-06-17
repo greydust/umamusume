3994cdbab9f11db197fe6b042ddb9bfb623d72b0
hakii6 <ty5220669123@gmail.com>
2021-06-17 13:57:00 +0800

    Updates

---
+++ precache-manifest.f2676dda6100b68179a84f74d3f04a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acfe28e2ec7997918df",
+    "url": "/umamusume/static/css/main.43f73a0b.chunk.css"
+  },
+  {
+    "revision": "6acfe28e2ec7997918df",
+    "url": "/umamusume/static/js/main.6acfe28e.chunk.js"
+  },
+  {
+    "revision": "027faa8ad27d15dbc0bf",
+    "url": "/umamusume/static/css/1.c8ddee2b.chunk.css"
+  },
+  {
+    "revision": "027faa8ad27d15dbc0bf",
+    "url": "/umamusume/static/js/1.027faa8a.chunk.js"
+  },
+  {
+    "revision": "4ab5a9fff5d9bdc746a2",
+    "url": "/umamusume/static/js/runtime~main.4ab5a9ff.js"
+  },
+  {
+    "revision": "5b605ed7d6531c5648aee38f2b5ad806",
+    "url": "/umamusume/index.html"
+  }
+];
