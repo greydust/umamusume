1ede365acff1e87ed81565fb84bec991c0da964c
Cheng-Yang Tsai <greydust@gmail.com>
2021-06-24 23:11:13 +0800

    add course slope data

--- script/extract_assets.py
+++ script/extract_assets.py
@@ -1,3 +1,5 @@
+import json
+import math
 import os
 import UnityPy
 import sqlite3
@@ -13,14 +15,14 @@ connection = sqlite3.connect(os.path.join(asset_folder, "meta"))
 connection.row_factory = db_dict_factory
 current = connection.cursor()
 
-current.execute('SELECT n as name, h as hash from a WHERE n LIKE "chara/chr%/chr\_icon\_____" ESCAPE "\\"')
-for item in current.fetchall():
-  image_name = (item["name"].split("/")[-1]).split("_")[2]
-  assets = UnityPy.load(os.path.join(asset_folder, "dat", item["hash"][:2], item["hash"]))
-  for obj in assets.objects:
-    if obj.type in [UnityPy.enums.ClassIDType.Texture2D, UnityPy.enums.ClassIDType.Sprite]:
-      image = obj.read().image
-      image.save(os.path.join("..\\public\\static\\image\\character\\portrait", image_name + ".png"))
+def extract_character_portrait():
+  current.execute('SELECT n as name, h as hash from a WHERE n LIKE "chara/chr%/chr\_icon\_____" ESCAPE "\\"')
+  for item in current.fetchall():
+    image_name = (item["name"].split("/")[-1]).split("_")[2]
+    assets = UnityPy.load(os.path.join(asset_folder, "dat", item["hash"][:2], item["hash"]))
+    for obj in assets.objects:
+      if obj.type in [UnityPy.enums.ClassIDType.Texture2D, UnityPy.enums.ClassIDType.Sprite]:
+        image = obj.read().image
+        image.save(os.path.join("..\\public\\static\\image\\character\\portrait", image_name + ".png"))
 
-
-
+extract_character_portrait()
