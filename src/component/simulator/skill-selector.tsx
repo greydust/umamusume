a78ad2b0a3a9db1591b01e400481271dff5b23c4
Cheng-Yang Tsai <greydust@gmail.com>
2021-07-10 00:18:03 +0800

    base skill selector

--- src/component/simulator/skill-selector.tsx
+++ src/component/simulator/skill-selector.tsx
@@ -1,6 +1,14 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import TreeSelect from 'rc-tree-select';
 
-import { LocalizationData } from '../../library/common';
+import { LocalizationData, SkillData } from '../../library/common';
+
+import 'rc-tree-select/assets/index.less';
+
+import skillJson from '../../db/skill.json';
+
+const skills = skillJson as SkillData[];
 
 interface IProps {
   localization: LocalizationData,
@@ -13,16 +21,34 @@ interface IState {
 }
 
 class SkillSelector extends Component<IProps, IState> {
+  private _skillSelectData: { key: string, label: string, value: string, disabled: boolean }[];
+
   constructor(props: IProps) {
     super(props);
     this.state = {
     };
+
+    this._skillSelectData = _.map(skills, (skill) => ({
+      key: skill.id,
+      label: skill.name,
+      value: skill.id,
+      disabled: false,
+    }));
   }
 
   render() {
     return (
-      <>
-      </>
+      <TreeSelect
+        className="check-select"
+        dropdownPopupAlign={{
+          overflow: { adjustY: 0, adjustX: 0 },
+          offset: [0, 2],
+        }}
+        treeLine
+        autoClearSearchValue
+        treeData={this._skillSelectData}
+        showCheckedStrategy={TreeSelect.SHOW_PARENT}
+      />
     );
   }
 }
