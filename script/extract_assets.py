7f3a4b9952b5d7a2d67de08a334adb052083e2e2
Cheng-Yang Tsai <greydust@gmail.com>
2021-07-11 03:44:39 +0800

    extract skill icons

--- script/extract_assets.py
+++ script/extract_assets.py
@@ -22,15 +22,26 @@ connection = sqlite3.connect(os.path.join(asset_folder, "meta"))
 connection.row_factory = db_dict_factory
 current = connection.cursor()
 
-def extract_character_portrait():
+def extract_character_portraits():
   current.execute('SELECT n as name, h as hash from a WHERE n LIKE "chara/chr%/chr\_icon\_____" ESCAPE "\\"')
   for item in current.fetchall():
-    image_name = (item["name"].split("/")[-1]).split("_")[2]
+    image_name = (item["name"].split("/")[-1]).split("_")[-1]
     assets = UnityPy.load(os.path.join(asset_folder, "dat", item["hash"][:2], item["hash"]))
     for obj in assets.objects:
       if obj.type in [UnityPy.enums.ClassIDType.Texture2D, UnityPy.enums.ClassIDType.Sprite]:
         image = obj.read().image
         image.save(os.path.join(base_dir, "..\\public\\static\\image\\character\\portrait", image_name + ".png"))
+
+def extract_skill_icons():
+  current.execute('SELECT n as name, h as hash from a WHERE n LIKE "outgame/skillicon/utx\_ico\_skill\_%" ESCAPE "\\"')
+  for item in current.fetchall():
+    image_name = (item["name"].split("/")[-1]).split("_")[-1]
+    assets = UnityPy.load(os.path.join(asset_folder, "dat", item["hash"][:2], item["hash"]))
+    for obj in assets.objects:
+      if obj.type in [UnityPy.enums.ClassIDType.Texture2D, UnityPy.enums.ClassIDType.Sprite]:
+        image = obj.read().image
+        image.save(os.path.join(base_dir, "..\\public\\static\\image\\skill", image_name + ".png"))
+
 
 def extract_course_param():
   current.execute('SELECT n as name, h as hash from a WHERE n LIKE "race/courseeventparam/%/pfb\_prm\_race%" ESCAPE "\\"')
@@ -60,6 +71,7 @@ def extract_course_slope():
                 with open(os.path.join(base_dir, "../temp" , basename), "w", newline='\n') as fp:
                   json.dump(slope_pers, fp, indent=2, sort_keys=True)
 
-extract_character_portrait()
+extract_character_portraits()
+extract_skill_icons()
 # extract_course_param()
 # extract_course_slope()
